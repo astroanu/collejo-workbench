4428f6f37f8a18d85aa0fb98f334a31e954b5d61
Anuradha Jayathilaka <astroanu2004@gmail.com>
2016-09-17 15:26:11 +0000

    config queue and events handlers

--- config/collejo.php
+++ config/collejo.php
@@ -2,14 +2,36 @@
 
 return [
 
+	// send emails on
+	'emails' => [
+		'new_user_password_create_request' => [
+			'student' => true,
+			'guardian' => true,
+			'admin' => true,
+			'employee' => true
+		]
+	],
+
 	// global pagination configuration
 	'pagination' => [
 		'perpage' => 10
 	],
 
+	// assets
+	'assets' => [
+		'minified' => true
+	],
+
 	// app caching
 	'caching' => [
-		// repository search result cache ttl
-		'repository_search' => 30
+		// cache each user's permissions
+		'user_permissions' => 30,
+		// cache repository search criteria results. relationship are not cached
+		'search_criteria' => 30
+	],
+
+	'modules' => [
+		// check module permissions in database on every request
+		'check_permissions' => false
 	]
 ];
