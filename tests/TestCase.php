e1242390d2e56bb76649aa6678297079d3d19616
astroanu <astroanu2004@gmail.com>
2018-02-14 18:39:39 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,10 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as IlluminateTestCase;
 use Collejo\App\Contracts\Console\Kernel;
 use Collejo\App\Contracts\Http\Kernel as Http;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestCase as IlluminateTestCase;
 use Illuminate\Http\Request;
 
 abstract class TestCase extends IlluminateTestCase
@@ -26,7 +26,7 @@ abstract class TestCase extends IlluminateTestCase
     public function createApplication()
     {
         $this->baseUrl = env('APP_URL');
-        
+
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
@@ -57,46 +57,55 @@ abstract class TestCase extends IlluminateTestCase
     public function artisan($command, $parameters = [])
     {
         return $this->code = $this->app[Kernel::class]->call($command, $parameters);
-    } 
+    }
 
     public function assertArrayValuesEquals($a, $b)
     {
         $a = !is_array($a) ? $a->toArray() : $a;
         $b = !is_array($b) ? $b->toArray() : $b;
 
-        if (isset($a['id'])) { unset($a['id']); }        
+        if (isset($a['id'])) {
+            unset($a['id']);
+        }
 
-        if (isset($b['id'])) { unset($b['id']); }
+        if (isset($b['id'])) {
+            unset($b['id']);
+        }
 
-        foreach($b as $k => $v) {
+        foreach ($b as $k => $v) {
             if (isset($a[$k])) {
                 if ($v != $a[$k]) {
                     return $this->assertTrue(false);
                 }
             }
-        } 
-        
-        return $this->assertTrue(true);       
+        }
+
+        return $this->assertTrue(true);
     }
 
-    public function assertArrayEquals($a, $b) {
+    public function assertArrayEquals($a, $b)
+    {
         $a = !is_array($a) ? $a->toArray() : $a;
         $b = !is_array($b) ? $b->toArray() : $b;
 
-        if (isset($a['id'])) { unset($a['id']); }        
+        if (isset($a['id'])) {
+            unset($a['id']);
+        }
 
-        if (isset($b['id'])) { unset($b['id']); }
+        if (isset($b['id'])) {
+            unset($b['id']);
+        }
 
         if (count(array_diff_assoc($a, $b))) {
             return $this->assertTrue(false);
         }
 
-        foreach($a as $k => $v) {
+        foreach ($a as $k => $v) {
             if ($v !== $b[$k]) {
                 return $this->assertTrue(false);
             }
         }
 
         return $this->assertTrue(true);
-    }          
+    }
 }
