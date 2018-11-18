128d61a51cad70d969a297f1c91f90fcc971f518
Anuradha <astroanu2004@gmail.com>
2018-11-18 23:32:45 +0530

    Image uploader and student details save

--- config/collejo.php
+++ config/collejo.php
@@ -98,4 +98,59 @@ return [
 
         'fe_asset_cache_ttl' => env('COLLEJO_FE_ASSETS_CACHE_TTL', 0),
     ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Image Uploader
+    |--------------------------------------------------------------------------
+    |
+    | When uploading image Collejo will use the following pre-defined folders.
+    | disk - name of the disk as defined in filesystems.php
+    | path = subfolder path in the disk
+    | permission - user permissions required to upload files
+    | mime_types - valid mime types
+    | max_size - allowed max file size
+    | resize - image files will be automatically resized to the given width and height.
+    |
+    */
+
+    'file_uploader' => [
+
+        'buckets' => [
+            'student_pictures' => [
+                'disk' => 'local',
+                'path' => '/student_pictures',
+                'permissions' => ['edit_student'],
+                'mime_types' => ['image/jpeg', 'image/png'],
+                'max_size' => 1000,
+                'resize' => [
+                    'small' => [200, 200],
+                    'medium' => [600, 600]
+                ]
+            ],
+            'employee_pictures' => [
+                'disk' => 'local',
+                'path' => '/employee_pictures',
+                'permissions' => ['edit_employee'],
+                'mime_types' => ['image/jpeg', 'image/png'],
+                'max_size' => 1000,
+                'resize' => [
+                    'small' => [200, 200],
+                    'medium' => [600, 600]
+                ]
+            ],
+            'employee_attachments' => [
+                'disk' => 'local',
+                'path' => '/employee_attachments',
+                'permissions' => ['edit_employee'],
+                'mime_types' => ['image/jpeg', 'image/png', 'application/pdf'],
+                'max_size' => 10000,
+                'resize' => [
+                    'small' => [200, 200],
+                    'medium' => [600, 600]
+                ]
+            ],
+        ]
+    ]
 ];
